1b89dd926503f92d48bea1a726db5d95af046970
RonDmit <137594555+RonDmit@users.noreply.github.com>
2023-09-14 19:27:49 +0500

    Задание лайт 3 задание второе задание

--- Python/Task3-lite/use_functions.py
+++ Python/Task3-lite/use_functions.py
@@ -33,7 +33,30 @@
 
 Для реализации основного меню можно использовать пример ниже или написать свой
 """
+account = 0
+purchase_history = []
+def add_money():
+    money = int(input("Введите сумму, которую хотите положить на счет: "))
+    global account
+    account += money
+    print(f'На вашем счету {account} рублей')
 
+def buy_product():
+    global account
+    product = input('Введите товар: ')
+    price = int(input('Введите цену за которую покупаете: '))
+    if price <= account:
+        print('Покупка совершена')
+        account -= price
+        print(f'На счету осталось {account} рублей')
+        purchase_history.append(f"{product} {price}")
+    else:
+        print("Денег не достаточно на счету")
+        print(f'На счету {account} рублей')
+def print_product():
+    print('Историй покупок:')
+    for i in purchase_history:
+        print(i)
 while True:
     print('1. пополнение счета')
     print('2. покупка')
@@ -42,12 +65,14 @@ while True:
 
     choice = input('Выберите пункт меню')
     if choice == '1':
-        pass
+        add_money()
     elif choice == '2':
-        pass
+        buy_product()
     elif choice == '3':
-        pass
+        print_product()
     elif choice == '4':
         break
     else:
         print('Неверный пункт меню')
+
+
